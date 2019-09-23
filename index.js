1c91535099472f60a308adf27145fea8ff62f277
marumisaki <beautiful0219bloom@gmail.com>
2019-09-23 20:50:00 +0900

    todoをtodosにしました

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ document.addEventListener("DOMContentLoaded", function () {
         target = '';
         //配列にinputタグで入力された内容を変数addTaskに入れる
         const task = document.new_task.addtask.value;
-        //todo配列にaddTaskをプッシュ
-        todo.push(task);
+        //todos配列にaddTaskをプッシュ
+        todos.push(task);
         console.log(task);
-        console.log(todo);
+        console.log(todos);
         document.getElementById('addtask').value = '';
-        for (let i = 0; i < todo.length; i++) {
-            html = '<td>' + i + '</td>' + '<td>' + todo[i] + '</td>' + '<td><input id="state" type="button" style="margin:2px;" value="完了"><input id="todo[del][i]" type="button" style="margin:2px;" value="削除"></td></tr>';
+        for (let i = 0; i < todos.length; i++) {
+            html = '<td>' + i + '</td>' + '<td>' + todos[i] + '</td>' + '<td><input id="state" type="button" style="margin:2px;" value="完了"><input id="todo[del][i]" type="button" style="margin:2px;" value="削除"></td></tr>';
         }
         target = document.getElementById('task_list');
         // ここでタスクを表示する
