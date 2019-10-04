5fa421335eb7d8e753d10b4ccaef83fe893164e0
marumisaki <beautiful0219bloom@gmail.com>
2019-10-04 22:05:52 +0900

    indexに初期値を入れ、インデントと()を直しました

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
+index = 0
 document.addEventListener("DOMContentLoaded", function () {
     //id=addbtnがクリックされたら
     document.getElementById("addbtn").addEventListener("click", function () {
-            const todos = [];
-            //配列にinputタグで入力された内容を変数addTaskに入れる
-            let task = document.getElementById("addtask").value;
-            console.log(task);
-            //todos配列にaddTaskをプッシュ
-            todos.push(task);
-            console.log(task);
-            console.log(todos);
-            task = '';
-            //tdタグ生成
-            let td1 = document.createElement('td');
-            td1.textContent = index;
+        const todos = [];
+        //配列にinputタグで入力された内容を変数addTaskに入れる
+        let task = document.getElementById("addtask").value;
+        console.log(task);
+        //todos配列にaddTaskをプッシュ
+        todos.push(task);
+        console.log(task);
+        console.log(todos);
+        task = '';
+        //tdタグ生成
+        let td1 = document.createElement('td');
+        td1.textContent = index;
 
-            let td2 = document.createElement('td');
-            td2.textContent = todos;
+        let td2 = document.createElement('td');
+        td2.textContent = todos;
 
-            let td3 = document.createElement('td');
-            let stateButton = document.createElement('button');
-            stateButton.textContent = '作業中';
-            td3.appendChild(stateButton);
+        let td3 = document.createElement('td');
+        let stateButton = document.createElement('button');
+        stateButton.textContent = '作業中';
+        td3.appendChild(stateButton);
 
-            let td4 = document.createElement('td');
-            let deleteButton = document.createElement('button');
-            deleteButton.textContent = '削除';
-            td3.appendChild(deleteButton);
+        let td4 = document.createElement('td');
+        let deleteButton = document.createElement('button');
+        deleteButton.textContent = '削除';
+        td3.appendChild(deleteButton);
 
-            //trタグ生成
-            let tr = document.createElement('tr');
-            tr.appendChild(td1);
-            tr.appendChild(td2);
-            tr.appendChild(td3);
-            tr.appendChild(td4);
+        //trタグ生成
+        let tr = document.createElement('tr');
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        tr.appendChild(td4);
 
-            let tbody = document.getElementById('list-body');
-            tbody.appendChild(tr);
+        let tbody = document.getElementById('list-body');
+        tbody.appendChild(tr);
 
-            index = index + 1;
-        }
+        index = index + 1;
     })
 })
